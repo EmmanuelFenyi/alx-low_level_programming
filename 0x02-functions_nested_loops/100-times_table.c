6e6e16dbc670a16b53ec6a7e47f4086b96f7cb33
Emmanuel Fenyi <kwamefemma@gmail.com>
2022-07-06 01:47:17 +0000

    Commit 100-times_table.c file 2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,38 +7,45 @@
 
 void print_times_table(int n)
 {
-	int num, mult, prod;
+	int i, j, res;
 
-	if (n >= 0 && n <= 15)
+	if (!(n > 15 || n < 0))
 	{
-		for (num = 0; num <= n; num++)
+		for (j = 0; j <= n; j++)
 		{
-			_putchar('0');
-
-			for (mult = 1; mult <= n; mult++)
+			res = (i * j);
+			if (j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-
-				_putchar((prod % 10) + '0');
 			}
+
+			if (res < 10 && j != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((res % 10) + '0');
+			}
+
+			else if (res >= 10 && res < 100)
+			{
+				_putchar(' ');
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+
+			else if (res >= 100 && j != 0)
+			{
+				_putchar((res / 100) + '0');
+				_putchar((res / 10) % 10 + '0');
+				_putchar((res % 10) + '0');
+			}
+
+			else
+				_putchar((res % 10) + '0');
 		}
+		_putchar('\n');
 	}
+}
+
 
